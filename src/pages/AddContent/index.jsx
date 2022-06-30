7867c8c4b73a30e4939811e33c8f14aa09801b28
berk-karabeyoglu <berkkarabeyoglu@gmail.com>
2022-06-30 18:34:01 +0300

    fixed wysiwyg editor

--- src/pages/AddContent/index.jsx
+++ src/pages/AddContent/index.jsx
@@ -34,7 +34,7 @@ const AddContent = ({ contentTypeID, contentTypeFields }) => {
     if (type === 'boolean') return <BooleanInputField field={field} />;
     if (type === 'timestamp') return <TimestampInputField field={field} />;
     if (type === 'file') return <FileInputField field={field} />;
-    if (type === 'html') return <StringInputField field={field} />;
+    if (type === 'html') return <HTMLInputField field={field} />;
   };
 
   const generateInitialValues = () => {
