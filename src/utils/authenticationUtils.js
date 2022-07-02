f883ca267c99e4de87ff94a7e7ad054969ff62e1
berk-karabeyoglu <berkkarabeyoglu@gmail.com>
2022-07-02 23:02:07 +0300

    bug fixes

--- src/utils/authenticationUtils.js
+++ src/utils/authenticationUtils.js
@@ -3,114 +3,111 @@ import axios from 'axios';
 
 // Access token yoksa logine yonlendir
 const validateAccessToken = () => {
-  const token = localStorage.getItem(AUTH_CONSTANTS.TOKEN_KEY);
-  if (!token) {
-    window.location = '/login';
-    return false;
-  }
+    const token = localStorage.getItem(AUTH_CONSTANTS.TOKEN_KEY);
+    if (!token) {
+        window.location = '/login';
+        return false;
+    }
 
-  return true;
+    return true;
 };
 
 const logOutHandle = () => {
-  localStorage.removeItem('access_token');
-  localStorage.removeItem('user_login');
-  window.location = '/login';
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('user_login');
+    window.location = '/login';
 };
 
 // Access token varsa dashboarda yonlendirmek icin
 const accessTokenChecker = () => {
-  const token = localStorage.getItem(AUTH_CONSTANTS.TOKEN_KEY);
-  if (token) {
-    window.location = '/admin';
-    return false;
-  }
-  return true;
+    const token = localStorage.getItem(AUTH_CONSTANTS.TOKEN_KEY);
+    if (token) {
+        window.location = '/admin';
+        return false;
+    }
+    return true;
 };
 
 // Email veya token olmadan reset password ekranına gidilememesini saglamak icin
 const queryStringValidationCheck = () => {
-  const { token, email } = new Proxy(
-    new URLSearchParams(window.location.search),
-    {
-      get: (searchParams, prop) => searchParams.get(prop),
-    }
-  );
-  return {
-    token,
-    email,
-  };
+    const { token, email } = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    return {
+        token,
+        email,
+    };
 };
 
 const login = (email, password, onError) => {
-  axios
-    .post(API.API_URL + '/auth/login', {
-      email,
-      password,
-      device_name: 'TEST',
-    })
-    .then(response => {
-      localStorage.setItem(
-        'access_token',
-        JSON.stringify({
-          userLogin: true,
-          token: response.data.token,
+    axios
+        .post(API.API_URL + '/auth/login', {
+            email,
+            password,
+            device_name: 'TEST',
         })
-      );
-      var role = response.data.data.role;
-      var edittedRole = role.charAt(0).toUpperCase() + role.slice(1);
+        .then(response => {
+            localStorage.setItem(
+                'access_token',
+                JSON.stringify({
+                    userLogin: true,
+                    token: response.data.token,
+                })
+            );
+            var role = response.data.data.role;
+            var edittedRole = role.charAt(0).toUpperCase() + role.slice(1);
 
-      localStorage.setItem(
-        'user_info',
-        JSON.stringify({
-          userID: response.data.data.id,
-          userName: response.data.data.name,
-          userRole: edittedRole,
+            localStorage.setItem(
+                'user_info',
+                JSON.stringify({
+                    userID: response.data.data.id,
+                    userName: response.data.data.name,
+                    userRole: edittedRole,
+                })
+            );
+            window.location = '/admin/content-types';
         })
-      );
-      window.location = '/admin/content-types';
-    })
-    .catch(error => {
-      onError(error.message);
-    });
+        .catch(error => {
+            onError(error.response.data.message);
+        });
 };
 
 const sendEmailForForgotPassword = (email, onSuccess, onError) => {
-  axios
-    .post(API.API_URL + '/auth/password/reset', {
-      email,
-    })
-    .then(response => {
-      onSuccess(response.data.message);
-    })
-    .catch(error => {
-      onError(error.response.data.message);
-    });
+    axios
+        .post(API.API_URL + '/auth/password/reset', {
+            email,
+        })
+        .then(response => {
+            onSuccess(response.data.message);
+        })
+        .catch(error => {
+            onError(error.response.data.message);
+        });
 };
 
 const resetPassword = (password, password_confirmation, onSuccess, onError) => {
-  const { token, email } = queryStringValidationCheck();
-  axios
-    .put(API.API_URL + '/auth/password', {
-      email,
-      password,
-      password_confirmation,
-      token,
-    })
-    .then(response => {
-      onSuccess(response.data.message);
-    })
-    .catch(error => {
-      onError(error.response.data.message);
-    });
+    const { token, email } = queryStringValidationCheck();
+    axios
+        .put(API.API_URL + '/auth/password', {
+            email,
+            password,
+            password_confirmation,
+            token,
+        })
+        .then(response => {
+            onSuccess(response.data.message);
+        })
+        .catch(error => {
+            onError(error.response.data.message);
+        });
 };
 
 export const authUtils = {
-  validateAccessToken,
-  accessTokenChecker,
-  queryStringValidationCheck,
-  login,
-  sendEmailForForgotPassword,
-  resetPassword,
-  logOutHandle,
+    validateAccessToken,
+    accessTokenChecker,
+    queryStringValidationCheck,
+    login,
+    sendEmailForForgotPassword,
+    resetPassword,
+    logOutHandle,
 };
