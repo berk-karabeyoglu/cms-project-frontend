c0a7118bf8ef9611d5d4b5feef5625cc4d9fbbc9
berk-karabeyoglu <berkkarabeyoglu@gmail.com>
2022-07-02 23:08:50 +0300

    typo fixes

--- src/components/ContentInputFields/HtmlInput/index.jsx
+++ src/components/ContentInputFields/HtmlInput/index.jsx
@@ -1,58 +1,57 @@
 import { Editor } from 'react-draft-wysiwyg';
 import '../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import React, {Component, useEffect, useState} from 'react';
-import { EditorState, ContentState, convertFromHTML, convertToRaw } from 'draft-js'
+import React, { Component, useEffect, useState } from 'react';
+import { EditorState, ContentState, convertFromHTML, convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import { useFormikContext } from 'formik';
 import './editor.css';
 const HTMLInputField = ({ field }) => {
-  const { setFieldValue } = useFormikContext();
-  const [editorState, setEditorState] = useState(EditorState.createEmpty())
-  const [loadedDefaultData, setLoadedDefaultData] = useState(false)
+    const { setFieldValue } = useFormikContext();
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const [loadedDefaultData, setLoadedDefaultData] = useState(false);
 
-  const onEditorStateChange = editorState => {
-    setFieldValue(
-      field.name,
-      draftToHtml(convertToRaw(editorState.getCurrentContent()))
-    );
-    setEditorState(editorState)
-  };
+    const onEditorStateChange = editorState => {
+        setFieldValue(field.name, draftToHtml(convertToRaw(editorState.getCurrentContent())));
+        setEditorState(editorState);
+    };
     useEffect(() => {
         if (field.value && loadedDefaultData === false) {
-            setEditorState(EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(field.value))))
+            setEditorState(
+                EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(field.value)))
+            );
             setLoadedDefaultData(true);
         }
     });
-  return (
-    <Editor
-        editorState={editorState}
-      toolbarClassName="toolbarClassName"
-      wrapperClassName="wrapperClassName"
-      editorClassName="editorClassName"
-      onEditorStateChange={onEditorStateChange}
-      placeholder="Enter your body here..."
-      toolbarStyle={{
-        // backgroundColor: 'rgb(23, 25, 35)',
-        color: 'black',
-        width: '100%',
-        height: '8rem',
-        border: '0.05rem solid white',
-        borderRadius: '1rem',
-        marginBottom: '1rem',
-      }}
-      editorStyle={{
-        border: '0.05rem solid white',
-        borderRadius: '1rem',
-        marginBottom: '2rem',
-        textAlign: 'center',
-        maxHeight: '300px',
-        padding: '1rem',
-        letterSpacing: '0.1rem',
-        maxWidth: '100%',
-        overflow: 'auto',
-      }}
-    />
-  );
+    return (
+        <Editor
+            editorState={editorState}
+            toolbarClassName="toolbarClassName"
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+            onEditorStateChange={onEditorStateChange}
+            placeholder="Enter your body here..."
+            toolbarStyle={{
+                backgroundColor: 'rgb(23, 25, 35)',
+                color: 'black',
+                width: '100%',
+                height: '8rem',
+                border: '0.05rem solid white',
+                borderRadius: '1rem',
+                marginBottom: '1rem',
+            }}
+            editorStyle={{
+                border: '0.05rem solid white',
+                borderRadius: '1rem',
+                marginBottom: '2rem',
+                textAlign: 'center',
+                maxHeight: '300px',
+                padding: '1rem',
+                letterSpacing: '0.1rem',
+                maxWidth: '100%',
+                overflow: 'auto',
+            }}
+        />
+    );
 };
 
 export default HTMLInputField;
